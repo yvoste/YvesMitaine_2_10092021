e5088c9ca74eed4ed3ed7acc9551b884b299fe73
yvoste <yves.mitaine@nordnet.fr>
2021-09-10 18:42:13 +0200

    retreive all items in ech category and write in csv

--- function.py
+++ function.py
@@ -1,9 +1,90 @@
+
+from math import ceil
 #####################
-# extract_id function
+# extract_categories function
 #####################
-def extract_id(elem):
+
+def extract_categories(soup_cat):
+    list_categories = []
+    list_category_url = []
+    list_csv_file = []
     try:
-        target = elem.find("a", attrs={"class": "title"})
-        return target['id']
+        list_elem = soup_cat.find("ul", class_="nav-list").find_all("a")
+        for elem in list_elem:
+            list_category_url.append(elem.get("href"))
+            list_categories.append(elem.string.strip())
+        # print(list_categories)
+        # print(list_category_url)    
+        return [list_categories, list_category_url]
     except:
-        return 'Nothing_found'
+        return 'error'
+
+#####################
+# extract_items function
+#####################
+
+def extract_items(soup_item):
+    list_items = []    
+    try:
+        list_elem = soup_item.find("ol", class_="row").find_all("a")
+        def_list = []
+        for ls in list_elem:
+            knil = ls.get("href").strip("./")
+            if knil in def_list:
+                continue
+            else:
+                def_list.append(knil)
+        # print(def_list)
+        return def_list
+    except:
+        return 'error'
+
+
+
+
+#####################
+# extract_final_data function
+#####################
+
+def extract_final_data(soup, site, url):
+    list_result = []
+    try:
+        tr_data = soup.find_all("tr")
+        res = {}
+
+        for i in range(len(tr_data)):
+            res[tr_data[i].find('th').text] = tr_data[i].find('td').text
+
+        res["title"]= soup.find("h1").text
+
+        res["product_description"] = soup.find("p", class_="").text
+
+        list_li = soup.find("ul", class_="breadcrumb")
+        content_li = list_li.findChildren()[4]
+        res["category"] = content_li.find("a").text
+
+        elem = soup.find("img")
+        img = elem['src'].strip("./")
+        res["image_url"] = '{}{}'.format(site, '/' + img)
+
+
+
+        list_result.append(url)
+        list_result.append(res["UPC"])
+        list_result.append(res["title"])
+        tax_in = res["Price (incl. tax)"].strip("£")
+        list_result.append(tax_in)
+        tax_ex = res["Price (excl. tax)"].strip("£")
+        list_result.append(tax_ex)
+        modify_1 = res["Availability"]
+        modify_2 = modify_1[modify_1.find('(') + 1: modify_1.find(')')]
+        modify_3 = modify_2.split(" ")
+        list_result.append(modify_3[0])
+        list_result.append(res["product_description"])
+        list_result.append(res["category"])
+        list_result.append(res["Number of reviews"])
+        list_result.append(res["image_url"])
+        # print(list_result)
+        return list_result
+    except:
+        return 'error'
--- main.py
+++ main.py
@@ -1,31 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import csv342 as csv
+from math import ceil
+from function import extract_categories, extract_items, extract_final_data
 
+# general paramters
 site = "http://books.toscrape.com"
-url = "http://books.toscrape.com/catalogue/sharp-objects_997/index.html"
-
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-tr_data = soup.find_all("tr")
-res = {}
-
-for i in range(len(tr_data)):
-    res[tr_data[i].find('th').text] = tr_data[i].find('td').text
-
-res["title"]= soup.find("h1").text
-
-res["product_description"] = soup.find("p", class_="").text
-
-list_li = soup.find("ul", class_="breadcrumb")
-content_li = list_li.findChildren()[4]
-res["category"] = content_li.find("a").text
-
-elem = soup.find("img")
-img = elem['src'].strip("./")
-res["image_url"] = '{}{}'.format(site, '/' + img)
-
+catalog = "http://books.toscrape.com/catalogue/"
+list_book_url = []
 en_tete = [
     "product_page_url",
     "universal_product_code",
@@ -39,23 +21,85 @@ en_tete = [
     "image_url"
 ]
 
-list_result = [url, res["UPC"], res["title"]]
-tax_in = res["Price (incl. tax)"].strip("£")
-list_result.append(tax_in)
-tax_ex = res["Price (excl. tax)"].strip("£")
-list_result.append(tax_ex)
-modify_1 = res["Availability"]
-modify_2 = modify_1[modify_1.find('(') + 1: modify_1.find(')')]
-modify_3 = modify_2.split(" ")
-list_result.append(modify_3[0])
-list_result.append(res["product_description"])
-list_result.append(res["category"])
-list_result.append(res["Number of reviews"])
-list_result.append(res["image_url"])
-# print(list_result)
+global_url = "http://books.toscrape.com"
 
+# filled list_categories
+page = requests.get(global_url)
+soup_cat = BeautifulSoup(page.content, 'html.parser')
 
-with open('./data/data.csv', 'w') as file_csv:
-    writer = csv.writer(file_csv, delimiter='|')
-    writer.writerow(en_tete)
-    writer.writerow(list_result)
+ret = extract_categories(soup_cat)
+list_categories = ret[0]
+list_categories.remove('Books') # remove the first item books
+list_category_url = ret[1]
+list_category_url.pop(0)   # remove the first item books
+            
+"""
+categories_url = []
+for category_url in list_category_url:
+    url_cat = 'http://books.toscrape.com/'+ category_url
+    page_cat = requests.get(url_cat)
+    soup_pag = BeautifulSoup(page_cat.content, 'html.parser')
+    val = check_paging(soup_pag)
+    print(val) 
+    if val == 1:
+        categories_url.append(url_cat)
+    else:
+        i = 1        
+        while i <= val:
+            if i == 1:
+                categories_url.append(url_cat)
+            else:
+                url_modifying = category_url.replace("index", "page_" + str(i))
+                categories_url.append(url_modifying)
+            i += 1
+
+print(categories_url)
+print(len(list_categories))
+print(list_categories)
+print(list_category_url)
+
+"""
+i = 0
+while i < len(list_categories):
+    url_cat = 'http://books.toscrape.com/'+ list_category_url[i]  #url category
+    page_cat = requests.get(url_cat)
+    soup_pag = BeautifulSoup(page_cat.content, 'html.parser')
+    c = soup_pag.find("form", class_="form-horizontal")
+    nb_item_by_cat = c.text.split()[0]
+    get_items = extract_items(soup_pag)  # list of item in first page of category
+    print(list_categories[i])
+    with open("./data/"+ list_categories[i] +".csv", 'w') as file_csv:  # create and open csv
+        writer = csv.writer(file_csv, delimiter='|')
+        writer.writerow(en_tete)
+        for get_item in get_items:
+            url_item = catalog + get_item  # url item 
+            poge = requests.get(url_item)
+            soup_item = BeautifulSoup(poge.content, 'html.parser')
+            data_item = extract_final_data(soup_item, site, url_item)   # data of item
+            print(data_item[1])        
+            writer.writerow(data_item)
+    
+
+    if int(nb_item_by_cat) > 20:
+        indice = ceil(int(nb_item_by_cat) / 20)
+        w = 2        
+        while w <= indice:            
+            url_modifying = url_cat.replace("index", "page-" + str(w))
+            print(url_modifying)
+            page_cat = requests.get(url_modifying)
+            soup_pag = BeautifulSoup(page_cat.content, 'html.parser')
+
+            get_items = extract_items(soup_pag)
+
+            with open("./data/"+ list_categories[i] +".csv", 'a') as file_csv:
+                writer = csv.writer(file_csv, delimiter='|')
+                for get_item in get_items:
+                    url_item = catalog + get_item
+                    poge = requests.get(url_item)
+                    soup_item = BeautifulSoup(poge.content, 'html.parser')
+                    data_item = extract_final_data(soup_item, site, url_item)                
+                    writer.writerow(data_item)
+                   
+            w += 1
+        
+    i += 1
